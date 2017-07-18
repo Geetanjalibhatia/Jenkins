bcd689e9b2948da71b5bc4346fb3b458243bea16
Geetanjalibhatia <Geetanjali_bhatia01@yahoo.com>
2017-07-18 16:44:34 +1000

    y

---
+++ dummy.bat
@@ -0,0 +1,50 @@
+@echo off
+rem ----------------------------------------------------------------------
+rem     Fill the current directory out with some dummy files & folders 
+rem
+rem     Usage: dummyFileFolder.bat [folderNumber] [fileNumber]
+rem
+rem             [folderNumber]  define number of folder will be 
+rem                             create (default=10)
+rem 
+rem             [fileNumber]    define number of files will be create 
+rem                             inside each folder (default=5)
+rem
+rem             for example: dummyFileFolder.bat 2 5
+rem             Fill current directory with 2 folders which have
+rem             5 files inside each them.
+rem ----------------------------------------------------------------------
+
+setlocal EnableDelayedExpansion
+set folderNo=%1
+set fileNo=%2
+
+IF NOT DEFINED folderNo set folderNo=10
+IF NOT DEFINED fileNo set fileNo=5
+
+echo.
+echo   Creation of %folderNo% folder(s) and %fileNo% file(s) started...
+echo.
+echo   Plese Wait...
+echo.
+echo.
+echo.
+
+for /l %%i in (1,1,%folderNo%) do call :myTask %fileNo%
+
+echo.
+echo **********************************************
+echo.
+echo   Task Finished Successfuly.
+echo.
+echo **********************************************
+echo.
+pause
+
+
+:myTask
+set fileNo=%1
+set folderName=%random%
+md %folderName%
+for /l %%i in (1,1,%fileNo%) do dir /q /s > %~dp0%folderName%\!random!.txt
+GOTO :eof
